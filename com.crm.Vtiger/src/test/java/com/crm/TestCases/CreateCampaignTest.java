5c8ad930a33132fbf1e208cda741500f7edafbbd
PC <PC@DESKTOP-LDUR2H9>
2022-03-23 17:19:50 +0530

    this is new class

---
+++ com.crm.Vtiger/src/test/java/com/crm/TestCases/CreateCampaignTest.java
@@ -0,0 +1,11 @@
+package com.crm.TestCases;
+
+import org.testng.annotations.Test;
+
+public class CreateCampaignTest {
+
+	@Test(groups="smokeTest")
+	public void createCamp() {
+		System.out.println("campaign created");
+	}
+}
