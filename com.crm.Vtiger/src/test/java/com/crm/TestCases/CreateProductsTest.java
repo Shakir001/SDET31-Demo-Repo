fc0ee762967aab0a499df4d8eeb13c96c4d474a0
PC <PC@DESKTOP-LDUR2H9>
2022-03-24 12:00:26 +0530

    2nd modification

---
+++ com.crm.Vtiger/src/test/java/com/crm/TestCases/CreateProductsTest.java
@@ -0,0 +1,11 @@
+package com.crm.TestCases;
+
+import org.testng.annotations.Test;
+
+public class CreateProductsTest {
+
+	@Test(groups="smokeTest")
+	public void createProduct() {
+		System.out.println("Products created successfully");
+	}
+}
